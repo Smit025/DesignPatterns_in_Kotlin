55ace2414354848bf5bfe2cfa9e32a99432ccae5
Smit <susmit025@gmail.com>
2023-08-21 17:57:25 +0530

    Behavioral Design Pattern: Iterator Pattern

--- src/main/kotlin/BehavioralPattern/IteratorPattern/IteratorMain.kt
+++ src/main/kotlin/BehavioralPattern/IteratorPattern/IteratorMain.kt
@@ -1,9 +1,9 @@
 package BehavioralPattern.IteratorPattern
 
-fun main(){
-    val list = mutableListOf<Int>(1,2,3,4,5,6)
+fun main() {
+    val list = mutableListOf<Int>(1, 2, 3, 4, 5, 6)
     val iterator = MyIterator(list)
-    while(iterator.hasNext()){
+    while (iterator.hasNext()) {
         print(iterator.next())
     }
 }
